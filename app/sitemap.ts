9a0df41ee46287a6b641de89b6fa5d7e4c7f5c66
stephennewman <44733427+stephennewman@users.noreply.github.com>
2025-12-30 10:07:24 -0500

    Update sitemap.xml to include terms, privacy, disclaimer pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,6 +13,24 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.3,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.3,
+    },
+    {
+      url: `${baseUrl}/disclaimer`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.3,
+    },
   ];
 
   // Property subdomain pages
